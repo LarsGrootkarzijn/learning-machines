37f408b4b8da8fba9353a59766df317fdd2d266b
Lars Grootkarzijn <larsgrootkarzijnn@gmail.com>
2025-06-17 16:42:37 +0200

    fix imports

--- catkin_ws/src/learning_machines/src/learning_machines/__init__.py
+++ catkin_ws/src/learning_machines/src/learning_machines/__init__.py
@@ -1,4 +1,4 @@
 from .test_actions import run_all_actions
-from .qlearning import example1, example2
+from .qlearning import run_all_actions, run_single_trial, run_experiment, real_life
 
 __all__ = ("run_all_actions","run_single_trial","run_experiment", "real_life")
